4a897f5266b19f960974568dfb9c43cfd4070f39
Ari <amindell@terpmail.umd.edu>
2018-02-06 01:20:02 -0500

    added main class, serial package, and arduino code

---
+++ Arduino Code/OSVArduino/OSVArduino.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+Serial.setTimeout(5);
+}
+byte val[4];
+void loop() {    
+  if(Serial.findUntil("FLOAT","\n")){
+      String s = Serial.readStringUntil("\n");
+      float f = s.toFloat();
+      String msg = "f: ";
+      Serial.println(msg+f);
+  }
+}
